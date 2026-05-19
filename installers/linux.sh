81a4d92bef13779280224697ea73a08d370f4e51
bademli <berkankiran@hotmail.com>
2026-05-19 13:51:06 +0300

    installer test

---
+++ installers/linux.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -e
+
+REPO_URL="https://github.com/magnum357i/mpv-subfinder"
+PLUGIN_NAME="subfinder"
+CONFIG_DIR="$HOME/.config/mpv"
+TMP_DIR="/tmp/gitmpv$PLUGIN_NAME"
+
+# checking dependices...
+
+dependencies=(git,curl)
+missing_dependencies=false
+
+for dep in "${dependencies[@]}"; do
+    if ! command -v "$dep" >/dev/null 2>&1; then
+        echo "Error! '$dep' command not found. Please install it first."
+	    missing_dependencies=true
+    fi
+done
+
+if [ "$missing_dependencies" = true ]; then
+    exit 1
+fi
+
+# checking config path...
+
+if [ ! -d "$CONFIG_DIR" ]; then
+    echo "Error! MPV config directory not found: $CONFIG_DIR"
+    exit 1
+fi
+
+# reset
+
+[ -d "$TMP_DIR" ] && rm -rf "$TMP_DIR"
+[ -d "$CONFIG_DIR/scripts/$PLUGIN_NAME" ] && rm -rf "$CONFIG_DIR/scripts/$PLUGIN_NAME"
+
+# install
+
+git clone --depth 1 "$REPO_URL" "$TMP_DIR"
+mkdir -p "$CONFIG_DIR/scripts"
+mkdir -p "$CONFIG_DIR/script-opts"
+mv "$TMP_DIR/scripts/$PLUGIN_NAME" "$CONFIG_DIR/scripts"
+[ ! -f "$CONFIG_DIR/script-opts/$PLUGIN_NAME.conf" ] && mv "$TMP_DIR/script-opts/$PLUGIN_NAME.conf" "$CONFIG_DIR/script-opts"
+
+echo "Done!"
---
+++ installers/windows.ps1
@@ -0,0 +1,89 @@
+$ErrorActionPreference = "Stop"
+
+$pluginName = "subfinder"
+$tempDir    = "gitmpv$pluginName"
+$repoLink   = "https://github.com/magnum357i/mpv-subfinder"
+
+function Path-RemoveIfExists {
+
+	param([string] $p)
+
+	if (Test-Path $p) { Remove-Item $p -Force -Recurse > $null }
+}
+
+function Path-CreateDirIfNotExists {
+
+	param([string] $p)
+
+	if (!(Test-Path $p)) { New-Item -ItemType Directory -Path $p > $null }
+}
+
+function Download-Repo {
+
+	param([string] $url, [string] $temp)
+
+	$tempPath = "$env:TEMP\$temp"
+
+	Path-RemoveIfExists -p $tempPath
+
+	if (Get-Command git -ErrorAction SilentlyContinue) {
+
+		Write-Host "Cloning..."
+		git clone --depth 1 --quiet $url $tempPath 2>&1
+	}
+	else {
+
+		Write-Host "Downloading..."
+		New-Item -ItemType Directory -Path $tempPath > $null
+		Invoke-WebRequest -Uri "$url/archive/refs/heads/main.zip" -OutFile "$tempPath\main.zip" > $null
+
+		if (Test-Path "$tempPath\main.zip") {
+
+			Write-Host "Extracting..."
+			Expand-Archive "$tempPath\main.zip" -DestinationPath $tempPath > $null
+			Move-Item "$tempPath\*-main\*" $tempPath -Force > $null
+			Remove-Item "$tempPath\*-main" -Force > $null
+			Remove-Item "$tempPath\main.zip" -Force > $null
+		}
+		else {
+
+			throw "Files not downloaded"
+		}
+	}
+}
+
+function Install-Plugin {
+
+	param([string] $temp, [string] $name)
+
+	if (Test-Path "$env:TEMP\$temp\scripts\$name\main.lua") {
+
+		Write-Host "Installing..."
+		Path-CreateDirIfNotExists -p "$env:APPDATA\mpv\scripts"
+		Path-CreateDirIfNotExists -p "$env:APPDATA\mpv\script-opts"
+		Path-RemoveIfExists -p "$env:APPDATA\mpv\scripts\$name"
+		if (!(Test-Path "$env:APPDATA\mpv\script-opts\$name.conf")) { Move-Item "$env:TEMP\$temp\script-opts\$name.conf" "$env:APPDATA\mpv\script-opts" -Force > $null }
+		Move-Item "$env:TEMP\$temp\scripts\$name" "$env:APPDATA\mpv\scripts" -Force > $null
+		Remove-Item "$env:TEMP\$temp" -Recurse -Force > $null
+	}
+	else {
+
+		throw "Files not found"
+	}
+}
+
+Write-Host "[PLUGIN]"
+
+try {
+
+	Download-Repo -url $repoLink -temp $tempDir
+	Install-Plugin -temp $tempDir -name $pluginName
+}
+catch {
+
+	Write-Host "Plugin installation failed:" -ForegroundColor Red
+	Write-Host $($_.Exception.Message) -ForegroundColor Red
+	Exit 1
+}
+
+Write-Host "Plugin is ready!" -ForegroundColor Green
